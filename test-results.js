c42ec7a42e85bdf4db00a845732658e0082ebcb8
Markandayan Perumandi <markandayan.p@zohocorp.com>
2026-05-10 07:54:53 +0530

    chore: remove venv and __pycache__ from git tracking, clean up stale test files

--- test-results.js
+++
@@ -1,9 +0,0 @@
-import { test, expect, devices } from '@playwright/test';
-
-test.use({
-  ...devices['Desktop Chrome'],
-});
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
-});
